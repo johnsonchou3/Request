300c7d0519ae7e41d755915a945117eb97fb0424
johnsonchou3 <81026263+johnsonchou3@users.noreply.github.com>
2021-08-11 15:07:18 +0800

    Add files via upload

---
+++ Request/Payload.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Request
+{
+    /// <summary>
+    /// Payload 的類別, 會轉成json 格式放到request
+    /// </summary>
+    public class Payload
+    {
+        /// <summary>
+        /// dtno 欄位
+        /// </summary>
+        public int dtno { get; set; }
+
+        /// <summary>
+        /// ftno 欄位
+        /// </summary>
+        public int ftno { get; set; }
+
+        /// <summary>
+        /// params 欄位
+        /// </summary>
+        public string @params { get; set; }
+
+        /// <summary>
+        /// keymap 欄位
+        /// </summary>
+        public string keyMap { get; set; }
+
+        /// <summary>
+        /// assignSpid 欄位
+        /// </summary>
+        public string assignSpid { get; set; }
+    }
+}
---
+++ Request/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Request
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Payload payload = new Payload();
+            payload.dtno = 5875162;
+            payload.ftno = 0;
+            payload.@params = "AssignID=2609;MTPeriod=0;DTMode=0;DTRange=720;DTOrder=1;MajorTable=M059;";
+            payload.keyMap = "";
+            payload.assignSpid = "";
+
+            var jsonpayload = JsonConvert.SerializeObject(payload);
+            var content = new StringContent(jsonpayload, Encoding.UTF8, "application/json");
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = (HttpResponseMessage)await httpClient.PostAsync("http://192.168.99.95:5000/api/customreport", content);
+            Console.WriteLine(response);
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
